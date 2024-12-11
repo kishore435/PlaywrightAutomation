f5e78ad81385fde705c8f1cda9ff94cb727c1cf7
kishore435 <kishorekumarR105@gmail.com>
2024-12-11 20:15:51 +0530

    Frames

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -1,16 +1,36 @@
 const {test,expect} = require('@playwright/test');
 
 
-test.only("Popup Validations", async({page})=>
+test("Popup Validations", async({page})=>
 {
  await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
  /* await page.goto("https://www.google.com/");
- await page.goBack();
- await page.goForward(); */
+ await page.goBack();//navigate back
+ await page.goForward(); */ //navigate forward
 
- await expect(page.locator("#displayed-text")).toBeVisible();
+ await expect(page.locator("#displayed-text")).toBeVisible();//to check elment is visible or not
  await page.locator("#hide-textbox").click();
- await expect(page.locator("#displayed-text")).toBeHidden();
+ await expect(page.locator("#displayed-text")).toBeHidden();//checking element is hidden or not
 
+
+});
+
+
+test.only("Handling pop-up", async ({page}) =>
+{
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    /* await page.locator("#confirmbtn").click();
+  
+    await page.on('dialog',dialog => dialog.dismiss());
+    await page .pause();
+    await page.locator("#mousehover").hover();
+ */
+
+    const framesPage = page.frameLocator("#courses-iframe");
+    await framesPage.locator("[href='lifetime-access']:visible").click();//this locator returns two elements, one is invisible, want to switch the element which is visble
+    const text = await framesPage.locator(".text h2").textContent();
+    console.log (text.split(' ')[1]);
+
+   
 
 });
