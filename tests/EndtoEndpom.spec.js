431b031a5eb34b270e62e7ee604b79a24207c6f2
kishore435 <kishorekumarR105@gmail.com>
2024-12-12 00:50:28 +0530

    pom Implementation

---
+++ tests/EndtoEndpom.spec.js
@@ -0,0 +1,34 @@
+const { test, expect } = require('@playwright/test');
+
+const { POManager } = require('../pageobjects/POManager');
+
+
+test('Add Product to Cart Test', async ({ page }) => {
+
+    const poManager = new POManager(page);
+    const productName = "ZARA COAT 3";
+    const userName = "kishorekumarr105@gmail.com";
+    const password = "1028Rahul#";
+
+    const loginPage = poManager.getLoginPage();
+    const dashboardPage = poManager.getDashboardPage();
+
+    await loginPage.goTo();
+    await loginPage.validLogin(userName, password);
+
+    await dashboardPage.searchProductAddCart(productName);
+    await dashboardPage.navigateToCart();
+
+    const cartPage = poManager.getCartPage();
+    await cartPage.VerifyProductIsDisplayed(productName);
+    await cartPage.Checkout();
+    const ordersReviewPage = poManager.getOrdersReviewPage();
+    await ordersReviewPage.searchCountryAndSelect("ind", "India");
+    const orderId = await ordersReviewPage.SubmitAndGetOrderId();
+    console.log(orderId);
+    await dashboardPage.navigateToOrders();
+    const ordersHistoryPage = poManager.getOrdersHistoryPage();
+    await ordersHistoryPage.searchOrderAndSelect(orderId);
+    expect(orderId.includes(await ordersHistoryPage.getOrderId())).toBeTruthy();
+
+});
