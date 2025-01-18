36cda8dcf483a85cebfe0a41186e714ddd49d8f5
kishore435 <kishorekumarR105@gmail.com>
2025-01-18 07:05:40 +0530

    new1

--- tests/UI/AngularPractice.spec.js
+++ tests/UI/AngularPractice.spec.js
@@ -12,7 +12,7 @@ test('playwright special locators',async ({page})=>
         await page.getByRole('link',{name:'Shop'}).click();
         await page.locator("app-card").filter({hasText:"Nokia Edge"}).getByRole("button").click();
         await page.goto("kishorekumar");
-        await page.goto("agian")
+        await page.goto("in feature branch")
         await page.pause();
 
     });
