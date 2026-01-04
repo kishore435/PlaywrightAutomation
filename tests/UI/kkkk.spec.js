a1becc0a0781cdb0cf3c0a6c2722ee02c1ca96ac
kishore435 <kishorekumarR105@gmail.com>
2026-01-04 18:30:07 +0530

    Added new assertion in kkk file

--- tests/UI/kkkk.spec.js
+++ tests/UI/kkkk.spec.js
@@ -5,12 +5,15 @@ test('Registration Test',async ({page})=>
        
     
         await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
-        await page.locator("#dropdown-class-example").selectOption("option2");
-        await page.waitForTimeout(3000);
-        const name = page.locator("//input[@placeholder='Enter Your Name']")
-        await page.locator("#checkBoxOption1").click();
-       await name.fill("sachin");
-       await expect(name).toHaveValue('sachin');
-       const prices = await page.locator("//table[@class='table-display']//tr/td[3]").allTextContents();
-       console.log(prices);
+    //     await page.locator("#dropdown-class-example").selectOption("option2");
+    //     await page.waitForTimeout(3000);
+    //     const name = page.locator("//input[@placeholder='Enter Your Name']")
+    //     await page.locator("#checkBoxOption1").click();
+    //    await name.fill("sachin");
+    //    await expect(name).toHaveValue('sachin');
+    //    const prices = await page.locator("//table[@class='table-display']//tr/td[3]").allTextContents();
+    //    console.log(prices);
+      const text =  await page.locator(".blinkingText").first().textContent();
+      await expect(text).toContain("Free Access to InterviewQues/ResumeAssistance/Material");
+      console.log(text);
     });
